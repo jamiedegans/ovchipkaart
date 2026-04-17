cb8113b663773e681eff6134b76cebfdc2040a3a
jamiedegans <1216596@student.roc-nijmegen.nl>
2026-04-17 16:04:30 +0200

    Reformat NSAutomaat and add stop() method
    Reformat NSAutomaat.java (indentation and brace placement) and add a stop() method that clears the oVChipkaart and marks the machine as not occupied. Also standardize formatting in aanvullenTot and aanvullenMet and update console messages. Corresponding compiled class file was updated.

--- src/ovchipkaart/NSAutomaat.java
+++ src/ovchipkaart/NSAutomaat.java
@@ -1,7 +1,7 @@
 package ovchipkaart;
 
 public class NSAutomaat
-{	 
+{
     private OVChipkaart oVChipkaart;
     private boolean isDeMachineBezet = false;
     private double stortBedrag;
@@ -13,28 +13,48 @@ public class NSAutomaat
 	isDeMachineBezet = true;
 
     }
-    
-    public void aanvullenTot(double HetBedragTeBehalen) {
-	if(isDeMachineBezet == true) {
-	 System.out.println("je huidige saldo is" + oVChipkaart.getSaldo());
-	 stortBedrag = HetBedragTeBehalen - oVChipkaart.getSaldo();
-	 oVChipkaart.setSaldo(HetBedragTeBehalen);
-	 
-	 System.out.println("jij wilt het hebben tot " + HetBedragTeBehalen);
-	 
-	 System.out.println("nieuw saldo is " + oVChipkaart.getSaldo());
-	 
-	 } else {
-	     System.out.println("de machine is nog bezet");
-	 } 
+
+    public void aanvullenTot(double HetBedragTeBehalen)
+    {
+	if (isDeMachineBezet == true)
+	{
+	    System.out.println("je huidige saldo is" + oVChipkaart.getSaldo());
+	    stortBedrag = HetBedragTeBehalen - oVChipkaart.getSaldo();
+	    oVChipkaart.setSaldo(HetBedragTeBehalen);
+
+	    System.out.println("jij wilt het hebben tot " + HetBedragTeBehalen);
+
+	    System.out.println("nieuw saldo is " + oVChipkaart.getSaldo());
+
+	} else
+	{
+	    System.out.println("de machine is nog bezet");
+	}
     }
-    
-    public void aanvullenMet(double HetBedragErBij) {
-	if(isDeMachineBezet == true) { 
-	    System.out.println( oVChipkaart.setSaldo( oVChipkaart.getSaldo() + HetBedragErBij));								
+
+    public void aanvullenMet(double HetBedragErBij)
+    {
+	if (isDeMachineBezet == true)
+	{
+	    System.out.println(oVChipkaart.setSaldo(oVChipkaart.getSaldo() + HetBedragErBij));
+
+	} else{
+	    System.out.println("de machine is nog bezet");
+	}
+    }
 	
-    } else {
-	     System.out.println("de machine is nog bezet");
-	 } 
+
+	    public void stop()
+	    {
+		if (isDeMachineBezet == true)
+		{
+		    
+		    oVChipkaart = null;
+		    isDeMachineBezet = false;
+		} else
+		{
+		    System.out.println("de machine was nooit bezet");
+		}
+
     }
 }
